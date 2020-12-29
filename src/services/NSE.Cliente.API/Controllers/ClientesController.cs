f1bce58ba16021696aac6e117fb96d0742b3e365
Wagner Santos <wagner.santos@blueshift.com.br>
2020-12-28 23:43:34 -0300

    Setup inicial da api de carrinho

--- src/services/NSE.Cliente.API/Controllers/ClientesController.cs
+++ src/services/NSE.Cliente.API/Controllers/ClientesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Cliente.API.Application.Commands;
 using NSE.Core.Mediator;
 using NSE.WebApi.Core.Controllers;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace NSE.Cliente.API.Controllers
 {
+    [Authorize]
     public class ClientesController : MainController
     {
         private readonly IMediatorHandler _mediatorHandler;
